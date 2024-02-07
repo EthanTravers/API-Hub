dc6586b84d44acd13a1546dea72a577e91e9a830
PlonkerJim <ethantravers2003@gmail.com>
2024-02-07 23:59:19 +0000

    Fixed naming of player login function

--- api_hub/functions/player/player_login.py
+++ api_hub/functions/player/player_login.py
@@ -6,5 +6,5 @@ import json, logging, os
 function = func.Blueprint()
 
 @function.route('playerLogin',auth_level=func.AuthLevel.FUNCTION, methods=['POST'])
-def playerInfoSet(req: func.HttpRequest) -> func.HttpResponse:
+def playerLogin(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse(body=json.dumps({'result':True}),mimetype='application/json')

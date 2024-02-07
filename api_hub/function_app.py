affd46994da7cd6c8249d6ae0002e126f64c24af
PlonkerJim <ethantravers2003@gmail.com>
2024-02-07 23:56:19 +0000

    Fixed Function Deployment

--- api_hub/function_app.py
+++ api_hub/function_app.py
@@ -1,5 +1,24 @@
 import azure.functions as func
-import datetime
-import json
-import logging
 
+app = func.FunctionApp()
+bp = func.Blueprint("test")
+error = "..."
+
+
+app.register_blueprint(bp)
+
+try:
+
+    from functions.player.player_login import function as player_login
+
+    function_list = [
+        player_login
+    ]
+
+    for function in function_list:
+        app.register_blueprint(function)
+
+
+except Exception as e:
+    import logging
+    error = str(e)

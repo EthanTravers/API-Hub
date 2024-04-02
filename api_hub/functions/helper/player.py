7bc796bd700595c572f878cdbf2b757b4f830098
EthanTravers <et2g21@soton.ac.uk>
2024-04-02 16:57:17 +0100

    Backend register function and helper functions started

--- api_hub/functions/helper/player.py
+++ api_hub/functions/helper/player.py
@@ -1,3 +1,5 @@
+import json
+
 def usernameValid(username):
     # Check characters are allowed [a-zA-Z0-9_]
     # Check includes at least 1 letter
@@ -10,3 +12,17 @@ def passwordValid(password):
         return "error"
     # Check password length is valid (len>4,len<20)
     # Check password contains at least 1 letter, 1 number, 1 special character (!?_@£€$%&)
+
+class User:
+    def __init__(self, userData):
+        '''
+
+        :param userData (dict): Dictionary of user data.
+        Format:
+        {
+        'username': str,
+        'email': str,
+        'password': str,
+        }
+        '''
+        self.playerData = userData

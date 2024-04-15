6110ce29efe339327f88a0d5e1675e75a06da508
Ethan <ethantravers2003@gmail.com>
2024-04-15 09:22:02 +0100

    unencrypted settings

--- api_hub/functions/player/user_register.py
+++ api_hub/functions/player/user_register.py
@@ -5,7 +5,7 @@ from azure.cosmos.exceptions import CosmosHttpResponseError
 import json, logging, os
 
 function = func.Blueprint()
-
+ 
 @function.route('userRegister',auth_level=func.AuthLevel.FUNCTION, methods=['POST'])
 def userRegister(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse(body=json.dumps({'result':True}),mimetype='application/json')
